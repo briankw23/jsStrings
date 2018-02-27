fcb6c330af62ab73add911a79ed0e01c3ce6c0b1
Brian Wright <briankw23@gmail.com>
2018-02-26 20:31:20 -0600

    completed exercise 2

--- strings.js
+++ strings.js
@@ -1,4 +1,11 @@
-//challegne 1
+//challenge 1
 
 var numOfSandwhiches = 8;
 console.log("numOfSandwhiches", numOfSandwhiches);
+
+//challenge 2
+
+var name = "brian"
+var string = "Hello " + name + " how are you doing today?"
+
+console.log("string:", string);
